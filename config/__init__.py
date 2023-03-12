2d453bd623dfe490202dd25c738d773fc6e4eb1e
Igor <14leviafan88@gmail.com>
2023-03-12 17:56:32 +0600

    Third commit

--- config/__init__.py
+++ config/__init__.py
@@ -2,9 +2,10 @@ from config.url import Url
 from config.playwright import Playwright
 from config.expectations import Expectations
 from pages.index_page import IndexPage
-
+from config.qase import Qase
 
 url = Url()
 playwright = Playwright()
 expectations = Expectations()
 index_page = IndexPage()
+qase = Qase()git
